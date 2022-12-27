f1ee3d2eff5a07b475b73d52597ffd0762445509
İsmail Üçel <ismailucel@gmail.com>
2022-12-27 20:42:36 +0300

    using props for showing different coins

--- cryptomarket/App.js
+++ cryptomarket/App.js
@@ -1,18 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
 import CoinItem from './src/components/CoinItem';
+import cryptocurrencies from './assets/data/cryptocurrencies.json';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CoinItem />
-	  <StatusBar style="light" />
-	</View> 
-});
-const styles = StyleSheet.create({
-	flex: 1,
-    backgroundColor: "#121212",
-    paddingTop: 50,
-	}
-});
+      <FlatList 
+        data={cryptocurrencies}
+        renderItem = {({item}) => <CoinItem marketCoin={item} />}
+      />
+      <StatusBar style="light" />
+    </View>
+  );
+	
