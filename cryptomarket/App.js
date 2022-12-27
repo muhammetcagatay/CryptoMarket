b52db000b7d00db43490772b270339458a76c851
İsmail Üçel <ismailucel@gmail.com>
2022-12-27 20:51:17 +0300

    testing coindetailed screen

--- cryptomarket/App.js
+++ cryptomarket/App.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
 import HomeScreen  from './src/screens/HomeScreen';
+import CoinDetaiedScreen from './src/screens/CoinDetailedScreen';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <HomeScreen />
+      <CoinDetaiedScreen />
       <StatusBar style="light" />
     </View>
   );
