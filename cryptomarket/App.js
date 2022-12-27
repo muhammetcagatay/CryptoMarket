34bb1a048943e1d75717aabaf08cac13df34c973
İsmail Üçel <ismailucel@gmail.com>
2022-12-27 20:43:20 +0300

    adding homescreen for navigation later on

--- cryptomarket/App.js
+++ cryptomarket/App.js
@@ -1,17 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
-import CoinItem from './src/components/CoinItem';
-import cryptocurrencies from './assets/data/cryptocurrencies.json';
+import HomeScreen  from './src/screens/HomeScreen';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList 
-        data={cryptocurrencies}
-        renderItem = {({item}) => <CoinItem marketCoin={item} />}
-      />
+      <HomeScreen />
       <StatusBar style="light" />
     </View>
   );
-	
