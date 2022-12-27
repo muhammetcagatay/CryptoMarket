c0012f577116bcddedd45c5d0288069df0950c59
İsmail Üçel <ismailucel@gmail.com>
2022-12-27 21:03:58 +0300

    updating PortfolioAsset

--- cryptomarket/src/atoms/PortfolioAsset.js
+++ cryptomarket/src/atoms/PortfolioAsset.js
@@ -9,3 +9,21 @@ export const allPortfolioBoughtAssets = selector({
         return jsonValue != null ? JSON.parse(jsonValue) : []
     }
 })
+
+export const allPortfolioBoughtAssetsFromAPI = selector({
+    key:'allPortfolioBoughtAssetsFromAPI',
+    get:async ({get})=>{
+        const boughtPortfolioAssets = get(allPortfolioBoughtAssetsInStorage)
+        const portfolioAssetsMarketData = await getWatchlistedCoins(1, boughtPortfolioAssets.map((portfolioAsset) => portfolioAsset.id).join(','))
+
+        const boughtAssets = boughtPortfolioAssets.map((boughtAsset) =>{
+            const portfolioAsset =portfolioAssetsMarketData.filter((item) => boughtAsset.id ===item.id)[0]
+            return {
+                ...boughtAsset,
+                currentPrice: portfolioAsset.current_price,
+                priceChangePercentage: portfolioAsset.price_change_percentage_24h
+            }
+        })
+        return boughtAssets.sort((item1,item2) => (item1.quantityBought * item1.currentPrice) < (item2.quantityBought * item2.currentPrice))
+    }
+})
